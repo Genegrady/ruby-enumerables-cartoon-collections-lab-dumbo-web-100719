1ba06e4d721af949c62653e17287598dfbdb655d
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-29 13:09:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(long_calls)
-  if long_calls.length >= 4
+  if any long_calls.length >= 4
     return true 
   elsif long_calls.length < 4
     return false
